386be0883294f1b80e6208475e5dfd57a76972e5
Lou Amadio <lou.amadio@gmail.com>
2015-04-30 09:24:53 -0700

    First checkin of driving app for universal

---
+++ Control/Control/Drive.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Input;
+using System.Threading;
+
+namespace Control
+{
+    public sealed partial class Drive : Page
+    {
+        private string connectionName = null;
+        private CancellationToken buttonCancel;
+
+        public Drive()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            connectionName = e.Parameter as string;
+            base.OnNavigatedTo(e);
+        }
+
+        private void sendCommandToBot(string command, string paramName, string value)
+        {
+            if (command != null && paramName != null && value != null)
+            {
+                string url = "http://" + connectionName + ":3000/bot?cmd=" + command + "&" + paramName + "=" + value;
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+                webRequest.BeginGetResponse(BotRequest_Async, null);
+
+            }
+        }
+
+        private void BotRequest_Async(IAsyncResult result)
+        {
+            var req = result.AsyncState as HttpWebRequest;
+            try
+            {
+                var resp = req.EndGetResponse(result);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void RotateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var el = sender as Button;
+            sendCommandToBot("rotate", "deg", el.Tag as string);
+
+        }
+
+        private void MoveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var el = sender as Button;
+            sendCommandToBot("move", "dst", el.Tag as string);
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ Control/Control/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Control
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NetworkName.Text))
+            {
+                MessageDialog dialog = new MessageDialog("Please enter an IP Address");
+                var v = dialog.ShowAsync();
+            }
+            else
+            {
+                this.Frame.Navigate(typeof(Drive), NetworkName.Text);
+            }
+        }
+    }
+}
